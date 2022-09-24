33fe88d3366b0e53bc2b1b6a0774bd7b0713cfd4
Bodipoh <berylodipoh@gmail.com>
2022-09-24 16:02:12 +0300

    Complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,40 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
+
+function About(props) {
+  let bio = props.bio;
+
+  if (bio) {
+    return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+
       {/* add your <Links /> component here */}
+
+      <Links github={props.github} linkedin = {props.linkedin} />
     </div>
   );
+  } 
+
 }
 
 export default About;
+
+
+
+//function About() {
+  //return (
+   // <div id="about">
+      //<h2>About Me</h2>
+      //<p>{bio}</p>
+     // <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      //{/* add your <Links /> component here */}
+      //<Links github={props.github} linkedin={props.linkedin} />
+   // </div>
+ // );
+//}
+
+//export default About
